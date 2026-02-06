1d75fe1cc35afc3f12236883daee59f4c840eaa5
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2026-02-06 16:01:36 +0100

    chore: class for api

--- src/api.ts
+++ src/api.ts
@@ -3,28 +3,37 @@ interface App {
   method: 'GET' | 'POST';
   headers: {
     'Content-Type': string;
-    'Authorization': string;
   };
   body: object;
 }
 
-async function apiConnect(app: App): Promise<any> {
-  const response = await fetch(requestApi(app));
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json();
-}
 
-export function requestApi(app: App): any {
-  return {
-    url: app.url,
-    method: app.method,
-    headers: {
-      ...app.headers,
-    },
-    body: JSON.stringify({
-      // Your request body here
-    }),
-  };
+export class ApiClient {
+  private authHeader: string;
+  constructor(authHeader: string) {
+    this.authHeader = authHeader;
+    console.log(this.authHeader);
+  }
+
+  async apiConnect(app: App): Promise<any> {
+    const response = await fetch(this.requestApi(app));
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  }
+
+  public requestApi(app: App) {
+    return {
+      url: app.url,
+      method: app.method,
+      headers: {
+        Authorization: this.authHeader,
+        ...app.headers,
+      },
+      body: JSON.stringify({
+        // Your request body here
+      }),
+    };
+  }
 }
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,7 +1,7 @@
-import { requestApi } from '../src/api';
+import { ApiClient, requestApi } from '../src/api';
 
 describe('API should', () => {
-  test('connect to API and get data', async() => {
+  test('connect to API and get data', async () => {
     const requestData = await requestApi({
       url: 'https://api.notifynl.nl/v2/notifications/email',
       method: 'POST',
@@ -22,4 +22,44 @@ describe('API should', () => {
       }),
     });
   });
+
+  test('creating a class', async () => {
+    const apiClient = new ApiClient('testkey');
+    const apiClient2 = new ApiClient('mijnjwt');
+    expect(apiClient).toBeTruthy();
+
+    expect(apiClient.requestApi({
+      body: {},
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      url: 'https://example.com',
+    })).toEqual({
+      body: '{}',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'testkey',
+      },
+      method: 'POST',
+      url: 'https://example.com',
+    });
+    expect(apiClient2.requestApi({
+      body: {},
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      url: 'https://example.com',
+    })).toEqual({
+      body: '{}',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'mijnjwt',
+      },
+      method: 'POST',
+      url: 'https://example.com',
+    });
+  });
+
 });

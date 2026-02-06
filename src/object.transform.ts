810cd1f1d1661e09d21ca3bc2a1005f3b0301f70
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2026-02-06 13:38:00 +0100

    chore: Refactor
    Slight refactor of object mapping.

--- src/object.transform.ts
+++ src/object.transform.ts
@@ -1,48 +1,42 @@
+interface mappingConfiguration {
+  /**
+   * The path (dot-separated, e.g. `path.to.my.key`) to the key to map
+   */
+  path: string;
+  /**
+   * The type of key. We only support date for now
+   */
+  type: 'date';
+  /**
+   * The input format. Two supported formats for now.
+   */
+  inputFormat: 'yyyy-mm-dd hh:mm:ss' | 'YYYYMM';
+  /**
+   * The date output format, a string value with this format will be mapped.
+   */
+  outputFormat: Intl.DateTimeFormatOptions;
+}
+
 interface configuration {
   email_address: string;
-  personalisation:{
-    [key: string]: string | {
-      path: string;
-      type: 'date';
-      inputFormat: 'yyyy-mm-dd hh:mm:ss' | 'YYYYMM';
-      outputFormat: Intl.DateTimeFormatOptions;
-    };
+  personalisation: {
+    /**
+     * If the value is a string, we assume there's a direct mapping of the value
+     * If an object is provided,
+     */
+    [key: string]: string | mappingConfiguration;
   };
 }
 
-// Idee voor nieuwe configuration interface
-// interface configuration {
-//   email_address: string;
-//   personalisation:{
-//     [key: string]: string | {
-//       path: string,
-//       type?: 'date',
-//       inputFormat: 'yyyy-mm-dd hh:mm:ss' | 'YYYYMM',
-//       outputFormat: Intl.DateTimeFormatOptions`
-//     };
-//   };
-// }
 
 export function objectTransform(configuration: configuration, object: any): configuration {
   let personalisation: { [key: string]: string } = {};
   for (let key in configuration.personalisation) {
     let objectValue;
-    console.log(configuration.personalisation[key]);
     if (typeof configuration.personalisation[key] === 'string') {
       objectValue = getByPath(object, configuration.personalisation[key]);
     } else {
-      if (configuration.personalisation[key].type === 'date') {
-        let objectDate: Date;
-        if (configuration.personalisation[key].inputFormat === 'yyyy-mm-dd hh:mm:ss') {
-          objectDate = stringToDate(getByPath(object, configuration.personalisation[key].path));
-        } else if (configuration.personalisation[key].inputFormat === 'YYYYMM') {
-          objectDate = periodeToDate(getByPath(object, configuration.personalisation[key].path));
-        } else {
-          throw new Error('Invalid input format');
-        }
-        objectValue = formatDatetime(objectDate, configuration.personalisation[key].outputFormat);
-
-      }
+      objectValue = dateStringMappedObjectValue(configuration.personalisation[key], object);
     }
     personalisation[key] = objectValue;
   }
@@ -50,6 +44,22 @@ export function objectTransform(configuration: configuration, object: any): conf
     email_address: getByPath(object, configuration.email_address),
     personalisation: personalisation,
   };
+}
+
+function dateStringMappedObjectValue(configuration: mappingConfiguration, object: any) {
+  if (configuration.type === 'date') {
+    let objectDate: Date;
+    if (configuration.inputFormat === 'yyyy-mm-dd hh:mm:ss') {
+      objectDate = stringToDate(getByPath(object, configuration.path));
+    } else if (configuration.inputFormat === 'YYYYMM') {
+      objectDate = periodeToDate(getByPath(object, configuration.path));
+    } else {
+      throw new Error('Invalid input format');
+    }
+    return formatDatetime(objectDate, configuration.outputFormat);
+  } else {
+    throw new Error('Invalid input type');
+  }
 }
 
 function getByPath(obj: any, path: string): any {
